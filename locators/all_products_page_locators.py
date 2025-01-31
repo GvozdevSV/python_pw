7709def18bc3aa80aab90b4b9984f071ccf88320
sgvozdev <agasfen82@yandex.ru>
2025-01-31 16:23:00 +0400

    добавлен тест Проверка отображения страницы все товары

--- locators/all_products_page_locators.py
+++ locators/all_products_page_locators.py
@@ -1,4 +1,10 @@
 
 
 class AllProductsPageLocators:
+    PAGE_TITLE = 'span[class="title"]'
     PRODUCT_TITLES = 'div[data-test="inventory-item-name"]'
+    IMAGES = '//img[@class="inventory_item_img"]'
+    DESCRIPTIONS = '//div[@data-test="inventory-item-desc"]'
+    PRISES = 'div[class="inventory_item_price"]'
+    ADD_TO_CART_BUTTONS = 'button[class="btn btn_primary btn_small btn_inventory "]'
+    CART_ICON = 'div[class="shopping_cart_container"]'
