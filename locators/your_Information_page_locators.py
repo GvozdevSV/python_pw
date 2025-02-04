9805c5e36da9ca8b9dc5ba2e57c6861e092cfcea
sgvozdev <agasfen82@yandex.ru>
2025-02-04 07:36:00 +0400

    добавлен тест Проверка заполнения полей страницы Информация о покупателе

--- locators/your_Information_page_locators.py
+++ locators/your_Information_page_locators.py
@@ -1,8 +1,9 @@
 
 class YourInformationPageLocators:
-
+    TITLE = 'span[class="title"]'
     FIRST_NAME_FIELD = 'input[placeholder="First Name"]'
     LAST_NAME_FIELD = 'input[placeholder="Last Name"]'
     POST_CODE_FIELD = 'input[placeholder="Zip/Postal Code"]'
     CANSEL_BUTTON = 'button[data-test="cancel"]'
     CONTINUE_BUTTON = 'input[data-test="continue"]'
+    ERROR = '//h3[@data-test="error"]'
