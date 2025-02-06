41d409d943ea8b4fe2286c068c5f064ab5c7e5b4
sgvozdev <agasfen82@yandex.ru>
2025-02-06 10:19:06 +0400

    чистка ветки gh-pages

--- data/urls.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-class Urls:
-    enter_url = os.getenv("BASE_URL")
--- locators/login_page_locators.py
+++
@@ -1,7 +0,0 @@
-class LoginPageLocators:
-
-    LOGIN_FIELD = 'Username'
-    PASSWORD_FIELD = 'Password'
-    SUBMIT_BUTTON = '//input[@id="login-button"]'
-    ERROR = '//h3[@data-test="error"]'
-
